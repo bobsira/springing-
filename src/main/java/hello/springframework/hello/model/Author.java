7f6b60c0374100834fd18e269876990367a7895b
sirasira <bob.sira@betika.com>
2019-05-28 12:07:04 +0300

    Added JPA First Example

--- src/main/java/hello/springframework/hello/model/Author.java
+++ src/main/java/hello/springframework/hello/model/Author.java
@@ -1,14 +1,19 @@
 package hello.springframework.hello.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String firstName;
     private String lastName;
 
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books = new HashSet<>();
 
     public Author() {}
--- src/main/java/hello/springframework/hello/model/Book.java
+++ src/main/java/hello/springframework/hello/model/Book.java
@@ -1,15 +1,22 @@
 package hello.springframework.hello.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String isbn;
     private String publisher;
 
+    @ManyToMany
+    @JoinTable(name = "authors_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors = new HashSet<>();
 
     public Book(){}
